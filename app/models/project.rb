3e2c6a8dd8ec748971eaad954a7385f3942e50e4
Marc Lainez <marc.lainez@gmail.com>
2011-05-18 06:27:02 +0200

    Add validation for owner role field and change label color when error

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,16 +1,17 @@
 class Project < ActiveRecord::Base
   belongs_to :user
-  
+
   validates_presence_of :user
   validates_presence_of :country_code
   validates_presence_of :description
   validates_presence_of :sector
   validates_presence_of :practices
   validates_presence_of :obstacles
-  
+  validates_presence_of :owner_role
+
   cattr_reader :per_page
   @@per_page = 3
-  
+
   index do
     description
     sector
